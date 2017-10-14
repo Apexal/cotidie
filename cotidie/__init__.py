d1ec0fcdb3e572e78ff57bac6cc323147e177174
Frank Matranga <thefrankmatranga@gmail.com>
2017-10-14 19:45:19 -0400

    Auth for actions over view

--- cotidie/__init__.py
+++ cotidie/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask, session, redirect, url_for, request
 app = Flask(__name__)
 
 from cotidie.database import db
-import cotidie.auth
 import cotidie.views
 
 db.create_all()
--- cotidie/views.py
+++ cotidie/views.py
@@ -1,12 +1,14 @@
 from cotidie import app
 from flask import request, render_template
 from cotidie.database import *
+from cotidie.auth import *
 
 @app.route("/")
 def index():
     return render_template("index.html", is_home=True)
 
 @app.route("/actions")
+@requires_auth
 def actions():
     actions = Action.query.all()
     return render_template("actions.html", actions=actions)
