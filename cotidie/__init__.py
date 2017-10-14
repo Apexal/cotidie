01cc885cd1aa46098ce242561291df9fd0b3b722
Frank Matranga <thefrankmatranga@gmail.com>
2017-10-14 18:14:37 -0400

    Basic HTTP auth

--- cotidie/__init__.py
+++ cotidie/__init__.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 app = Flask(__name__)
 
+import cotidie.auth
+
 @app.route("/")
 def index():
     app.logger.debug('A value for debugging')
