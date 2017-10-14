4caa26b2df2e0328035560e3d134d522c4d5d3f8
Frank Matranga <thefrankmatranga@gmail.com>
2017-10-14 19:54:12 -0400

    Specific action page

--- cotidie/views.py
+++ cotidie/views.py
@@ -8,7 +8,12 @@ def index():
     return render_template("index.html", is_home=True)
 
 @app.route("/actions")
+@app.route("/actions/<int:action_id>")
 @requires_auth
-def actions():
-    actions = Action.query.all()
-    return render_template("actions.html", actions=actions)
+def actions(action_id=None):
+    if action_id is None:
+        actions = Action.query.all()
+        return render_template("actions.html", actions=actions)
+    else:
+        action = Action.query.filter_by(id = action_id).first()
+        return render_template("action.html", action=action)
