fa2820f0b45ee570e95e9d8e6c11f219f8b9e774
Frank Matranga <thefrankmatranga@gmail.com>
2017-10-14 19:39:48 -0400

    Actions page start

--- cotidie/views.py
+++ cotidie/views.py
@@ -1,7 +1,12 @@
 from cotidie import app
-from flask import render_template
+from flask import request, render_template
 from cotidie.database import *
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html", is_home=True)
+
+@app.route("/actions")
+def actions():
+    actions = Action.query.all()
+    return render_template("actions.html", actions=actions)
